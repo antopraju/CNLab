20b06f36a1017ead128d8c42cdb8f71d3e8fba35
antopraju <anthonyprajwal11@gmail.com>
2017-01-31 10:42:54 +0530

    Update Belman.c

--- Belman.c
+++ Belman.c
@@ -1,3 +1,8 @@
+/*
+Given  a  graph,each  node  A  knows  the shortest  path  to  node  Z  and  node  A  can determine its shortest path to Z by calculating the minimum cost. Now when packet 
+flows through a path it incurs some cost to the network,find shortest paths from src to all nodes in the given graph using Bellman Ford 
+Algorithm.The graph may contain negative weight edges.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 int Bellman_Ford(int G[20][20] , int V, int E, int edge[20][2])
