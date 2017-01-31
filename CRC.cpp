9a565f18bda88db8e225ef4c54864207df58e1ab
antopraju <anthonyprajwal11@gmail.com>
2017-01-31 10:44:13 +0530

    Update CRC.cpp

--- CRC.cpp
+++ CRC.cpp
@@ -1,3 +1,6 @@
+/*
+5.Write a program for implementing the error detection technique for data transfer in unreliable network code using CRC (16-bits) Technique.
+*/
 #include <iostream>
 #include <vector>
 
